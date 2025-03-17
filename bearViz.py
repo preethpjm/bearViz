ab532d69fd85171c38dce305a9ebc1c3206cc4cf
preeth jwo martin <preethakapjm@gmail.com>
2025-03-18 00:27:04 +0530

    final change

--- bearViz.py
+++ bearViz.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
 # Title
 st.image("Logo1(BearViz).png", width=300)
-st.markdown("## Transform data into insights, effortlessly! 📊")
+st.markdown("### Transform data into insights, effortlessly! 📊")
 # File Upload
 uploaded_file = st.file_uploader("Upload **CSV**, **Excel**, **TXT**, or **PDF** File", type=["csv", "xlsx", "txt", "pdf"])
 
